ea48bb580e27c227d128ec4eff369e7fbc81d82d
Gabrie Roberto <152230629+Gabriel-Assis-22@users.noreply.github.com>
2025-09-16 22:34:17 -0300

    Initial commit

---
+++ src/test/java/Desafio_Vaga_Jacto_API/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Desafio_Vaga_Jacto_API.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
