be307ce5e0725c9c63ec4c458c6da2f512d6a783
Gabrie Roberto <152230629+Gabriel-Assis-22@users.noreply.github.com>
2025-09-16 22:42:57 -0300

    commit

--- src/main/java/Desafio_Vaga_Jacto_API/demo/repository/CarroRepository.java
+++ src/main/java/Desafio_Vaga_Jacto_API/demo/repository/CarroRepository.java
@@ -1,6 +1,9 @@
 package Desafio_Vaga_Jacto_API.demo.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class CarroRepository {
-
+    private JdbcT
 }
